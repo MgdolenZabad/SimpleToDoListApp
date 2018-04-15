766bdfd96b953866689975389437f83b572d46bb
Mgdolen <mgdolen.zabad@gmail.com>
2018-04-14 23:21:38 -0400

    adding the edit button and move the items and delete the item from the list

--- ToDoList/Controller/ToDoListViewController.swift
+++ ToDoList/Controller/ToDoListViewController.swift
@@ -10,29 +10,38 @@ import UIKit
 
 class ToDoListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
     
-    //MARK : Call the Item Objects
+    //MARK - Call the Item Objects
     var itemArray = [Item]()
     
     
-    //MARK : IBOutlets
+    //MARK - IBOutlets
     
     @IBOutlet weak var newItemTextField: UITextField!
     @IBOutlet weak var addButton: UIButton!
     @IBOutlet weak var itemTableView: UITableView!
+    @IBOutlet weak var editButton: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    //MARK : Set ToDoListViewController as UItableView and TextField Delegate and data Source
+    //MARK - Set ToDoListViewController as UItableView and TextField Delegate and data Source
         
         itemTableView.delegate = self
         itemTableView.dataSource = self
         newItemTextField.delegate = self
         
+    //MARK - Customize the corner radius
+        
+        newItemTextField.layer.cornerRadius = newItemTextField.frame.height / 2.0
+        addButton.layer.cornerRadius = addButton.frame.height / 2.0
+        
+
+
+        
     }
 
-    //MARK : IBActions
+    //MARK - IBActions
     
     
     @IBAction func addButtonPressed(_ sender: Any) {
@@ -47,10 +56,17 @@ class ToDoListViewController: UIViewController, UITableViewDataSource, UITableVi
         self.newItemTextField.text! = ""
         
         
+    }
+    
+    
+    @IBAction func editButtonPressed(_ sender: Any) {
+        
+        itemTableView.isEditing = !itemTableView.isEditing
         
     }
     
-    //MARK : TableView DataSource Methods
+    
+    //MARK - TableView DataSource Methods
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
@@ -80,4 +96,23 @@ class ToDoListViewController: UIViewController, UITableViewDataSource, UITableVi
         }
   
     }
+    
+    //MARK - moving the items
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        
+        let temp = itemArray[sourceIndexPath.row]
+        itemArray.remove(at: sourceIndexPath.row)
+        itemArray.insert(temp, at: sourceIndexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if (editingStyle == .delete) {
+            itemArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .left)
+            
+        }
+    }
 }
+
